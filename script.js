47b2275786f1814ed68417524f7122bc81ef928f
Slip1244 <nithilan4@gmail.com>
2019-11-08 16:33:36 -0600

    typo and hftw hover light logo

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function hover(element) {
+    element.setAttribute('src', 'images/hftw-light.png');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', 'images/hftw-dark.png');
+  }
+  
+  document.getElementById("dark-logo").setAttribute("src", "images/hftw-dark.png")
