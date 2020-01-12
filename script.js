d0a3c1995c942de8926077fbecc9379596986b57
Slip1244 <nithilan4@gmail.com>
2020-01-11 18:25:35 -0600

    png fallbacks for webp and venue page fixes

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+function canUseWebP() {
+  var elem = document.createElement('canvas');
+
+  if (!!(elem.getContext && elem.getContext('2d'))) {
+      // was able or not to get WebP representation
+      return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+  }
+
+  // very old browser like IE 8, canvas not supported
+  return false;
+}
+ 
+var webp = "png";
+
+window.onload = () => {
+  if (canUseWebP()) {
+    webp = "webp";
+  }
+  console.log(webp)
+  document.getElementById("logo-parent").style.backgroundImage = "url('images/" + webp + "/parallax1." + webp + "')"
+  document.getElementById("register2").style.backgroundImage = "url('images/" + webp + "/net." + webp + "')"
+  document.getElementById("register-footer").style.backgroundImage = "url('images/" + webp + "/net." + webp + "')"
+}
+
 function hover(element) {
-    element.setAttribute('src', 'images/hftw-light.webp');
-  }
-  
-  function unhover(element) {
-    element.setAttribute('src', 'images/hftw-dark.webp');
-  }
+  element.setAttribute('src', 'images/png/hftw-light.png');
+}
+
+function unhover(element) {
+  element.setAttribute('src', 'images/png/hftw-dark.png');
+}
--- venue/script.js
+++ venue/script.js
@@ -1,7 +1,29 @@
-function hover(element) {
-    element.setAttribute('src', '../images/hftw-light.webp');
-}
-
-function unhover(element) {
-    element.setAttribute('src', '../images/hftw-dark.webp');
-}
+function canUseWebP() {
+    var elem = document.createElement('canvas');
+  
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+        // was able or not to get WebP representation
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    }
+  
+    // very old browser like IE 8, canvas not supported
+    return false;
+  }
+   
+  var webp = "png";
+  
+  window.onload = () => {
+    if (canUseWebP()) {
+      webp = "webp";
+    }
+    console.log(webp)
+    document.getElementById("register2").style.backgroundImage = "url('../images/" + webp + "/net." + webp + "')"
+  }
+  
+  function hover(element) {
+    element.setAttribute('src', '../images/png/hftw-light.png');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', '../images/png/hftw-dark.png');
+  }
